18abf0c309bef0a65c11eab4a73b8d5ae331ba05
mkrumin <mkrumin@gmail.com>
2017-08-19 14:14:28 +0100

    Laser power computation

--- prepareHardware.m
+++ prepareHardware.m
@@ -1,6 +1,6 @@
 function hw = prepareHardware
 
-% This function will prepare the NI board(s
+% This function will prepare the NI boards
 
 adaptorVendor = 'ni';
 galvoDeviceID = 'Dev1';
@@ -11,6 +11,7 @@ laserDeviceID = 'Dev2';
 laserChannelID = 'ao0';
 
 
+
 s = daq.createSession(adaptorVendor);
 mirrorXChannel = s.addAnalogOutputChannel(galvoDeviceID, xChID, 'Voltage');
 mirrorYChannel = s.addAnalogOutputChannel(galvoDeviceID, yChID, 'Voltage');
@@ -18,5 +19,9 @@ mirrorYChannel = s.addAnalogOutputChannel(galvoDeviceID, yChID, 'Voltage');
 laserChannel = s.addAnalogOutputChannel(laserDeviceID, laserChannelID, 'Voltage');
 
 s.SampleRate = sampleRate;
+
+s.addTriggerConnection('external', 'Dev1/PFI0', 'StartTrigger');
+s.addTriggerConnection('external', 'Dev2/PFI0', 'StartTrigger');
+s.addClockConnection('Dev1/PFI1', 'Dev2/PFI1', 'ScanClock');
 
 hw.s = s;
--- prepareNextStim.m
+++ prepareNextStim.m
@@ -3,3 +3,34 @@ function prepareNextStim(hw, stimParams)
 % This function will precompute the waveforms for the mirrors and the
 % laser, load them on the boards and arm the DAQ session
 % the session will then be waiting for a trigger to start
+
+%% for debugging
+stimParams.nPoints = 3;
+stimParams.stimDuration = 0.1;
+stimParams.ML = [-1.7 1.7 2.5];
+stimParams.AP = [-2 -2 -3.5];
+stimParams.stimPower = [0.8 1 0.5];
+hw.s.sampleRate = 20e3;
+
+%%
+f = 40; % [Hz] basic frequency of stimulation, each point will be visited f times per second
+flightTime = 2e-3; % [sec] Time that takes galvo mirrors to hop from one point to another
+% During this time the laser should be shut down
+
+% define the time axis of the stimulus
+dt = 1/hw.s.sampleRate;
+t = dt:dt:stimParams.stimDuration;
+
+cycleDuration = 1/f;
+locationDuration = cycleDuration/stimParams.nPoints;
+
+locIdxVector = mod(ceil(t/locationDuration)-1, stimParams.nPoints)+1;
+
+fSine = 1/(locationDuration-flightTime);
+tCycle = mod(t, locationDuration);
+laserPower = (-cos(2*pi*fSine*(tCycle-flightTime))+1)/2.*...
+    (tCycle>=flightTime).*(tCycle<=locationDuration);
+laserPowerMultiplier = stimParams.stimPower(locIdxVector);
+laserPower = laserPower.*laserPowerMultiplier;
+
+plot(t, laserPower, t, locIdxVector/3)
