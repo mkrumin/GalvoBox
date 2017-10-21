2d1cd1b541e966148906275634b302b4b218c0dc
Michael Krumin <mkrumin@gmail.com>
2017-10-21 02:34:05 +0100

    prepareHardware.m updated

--- prepareHardware.m
+++ prepareHardware.m
@@ -3,11 +3,11 @@ function hw = prepareHardware
 % This function will prepare the NI boards
 
 adaptorVendor = 'ni';
-galvoDeviceID = 'Dev1';
+galvoDeviceID = 'Dev2';
 xChID = 'ao0';
 yChID = 'ao1';
 sampleRate = 20e3;
-laserDeviceID = 'Dev2';
+laserDeviceID = 'Dev1';
 laserChannelID = 'ao0';
 
 s = daq.createSession(adaptorVendor);
@@ -16,15 +16,15 @@ mirrorYChannel = s.addAnalogOutputChannel(galvoDeviceID, yChID, 'Voltage');
 
 laserChannel = s.addAnalogOutputChannel(laserDeviceID, laserChannelID, 'Voltage');
 
-s.SampleRate = sampleRate;
+s.Rate = sampleRate;
 
 s.addTriggerConnection('external', 'Dev1/PFI0', 'StartTrigger');
 s.addTriggerConnection('external', 'Dev2/PFI0', 'StartTrigger');
-s.addClockConnection('Dev1/PFI1', 'Dev2/PFI1', 'ScanClock');
+s.addClockConnection('Dev2/PFI1', 'Dev1/PFI1', 'ScanClock');
 
 
 tr = daq.createSession('ni');
-tr.addDigitalChannel('Dev1', 'PFI5', 'OutputOnly');
+tr.addDigitalChannel('Dev2', 'Port0/Line0', 'OutputOnly');
 tr.outputSingleScan(0);
 
 % this is the daq session for the galvos and the laser
