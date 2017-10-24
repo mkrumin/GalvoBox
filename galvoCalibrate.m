5044c63218a1a38bab5fb8dfe2ad9e577355fd9c
Michael Krumin <mkrumin@gmail.com>
2017-10-24 14:48:35 +0100

    Automatic calibration with test

--- galvoCalibrate.m
+++ galvoCalibrate.m
@@ -16,6 +16,8 @@ set(hAxis, 'CLim', [0 255]);
 
 % this is also specific to ZCAMP3
 set(hAxis, 'XDir', 'reverse');
+set(hAxis, 'NextPlot', 'add');
+hSpot = plot(hAxis, 1, 1, 'ro');
 
 FOV = 8; %[mm] this is the field of view of the camera's full frame
 
@@ -25,17 +27,20 @@ pause(1);
 backFrame = fliplr(getsnapshot(vid));
 
 vLaser  = 2.5;
-vRangeX = -1:1;
-vRangeY = -1:1;
+vRangeX = -2:2;
+vRangeY = -2:3;
 [V.x, V.y] = meshgrid(vRangeX, vRangeY);
 pos = struct('x', nan(size(V.x)), 'y', nan(size(V.y)));
 
 nPoints = numel(V.x);
 for iPoint = 1:nPoints
     hw.s.outputSingleScan([V.x(iPoint) V.y(iPoint) vLaser])
-    pause(2); % wait until the galvos actually move (might be unnecessary)
+    pause(0.3); % wait until the galvos actually move (might be unnecessary)
     frame = getsnapshot(vid);
-    [pos.x(iPoint), pos.y(iPoint)] = findSpot(fliplr(frame));
+    [pos.x(iPoint), pos.y(iPoint)] = findSpot(frame);
+    hSpot.XData = pos.x(iPoint);
+    hSpot.YData = pos.y(iPoint);
+    pause(0.8);
 %     [pos.x(iPoint), pos.y(iPoint)] = findSpot(hAxis);
 end
 
@@ -46,21 +51,20 @@ p2vTransform = getTransform(pos, V);
 % plot(V.x(:), V.y(:), 'o', vEst(:,1), vEst(:, 2), 'x')
 
 %% testing calibration
-% h = figure;
-% im = imagesc(backFrame);
-% for iPoint = 1:5
-%     im.CData = backFrame;
-%     axis equal tight;
-%     x = ceil(rand*1024);
-%     y = ceil(rand*1024);
-%     hold on;
-%     plot(x, y, 'ro');
-%     vxy = [x y 1]*p2vTransform;
-%     hw.s.outputSingleScan([vxy, vLaser])
-%     pause(3); % wait until the galvos actually move (might be unnecessary)
-%     frame = fliplr(getsnapshot(vid));
-%     im.CData = frame;
-%     pause(3);
-% end
-% 
-% 
+
+for iPoint = 1:20
+    x = ceil(rand*800)+100;
+    y = ceil(rand*800)+100;
+    hSpot.XData = x;
+    hSpot.YData = y;
+    hSpot.Marker = 'x';
+%     hSpot.MarkerSize = 10;
+    pause(0.5)
+    vxy = [x y 1]*p2vTransform;
+    hw.s.outputSingleScan([vxy, vLaser])
+    pause(1); % wait until the galvos actually move (might be unnecessary)
+end
+
+% park the mirrors, switch off the laser
+hw.s.outputSingleScan([5 5 0]);
+
