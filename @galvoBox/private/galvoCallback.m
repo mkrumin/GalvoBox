39dd902c09b9084c89af0d7cb61a3449ddce4377
Michael Krumin <mkrumin@gmail.com>
2017-12-21 22:33:51 +0000

    Error handling during the UDP callback

--- @galvoBox/private/galvoCallback.m
+++ @galvoBox/private/galvoCallback.m
@@ -1,65 +1,78 @@
 function galvoCallback(src, event, obj)
 
-t = clock;
-obj.nUDPs = obj.nUDPs + 1;
-timeStamp = datestr(t, 'yyyy-mm-dd HH:MM:SS.FFF');
-ip=src.DatagramAddress;
-port=src.DatagramPort;
-data=fread(src);
-str=char(data');
-obj.UDPLog(obj.nUDPs).timeStamp = datenum(t);
-obj.UDPLog(obj.nUDPs).msg = str;
-fprintf('[galvoUDP] [%s] Received \n%sfrom %s:%d\n', timeStamp, str, ip, port);
-
 try
-    info=parseMessage(str);
-catch
-    info.instruction = 'Failed to parse';
-end
-
-% update remote IP to that of the sender
-src.RemoteHost = ip;
-src.RemotePort = port;
-
-switch info.instruction
-    case 'hello'
-        % this is just to check communication
-        timeStamp = datestr(clock, 'yyyy-mm-dd HH:MM:SS.FFF');
-        fprintf('[galvoUDP] [%s] Sending ''%s ...'' to %s:%d\n', timeStamp, info.instruction, ip, port);
-        fwrite(src, data);
-    case 'ExpStart'
-        obj.startExperiment(info.ExpRef);
-        timeStamp = datestr(clock, 'yyyy-mm-dd HH:MM:SS.FFF');
-        fprintf('[galvoUDP] [%s] Sending ''%s ...'' to %s:%d\n', timeStamp, info.instruction, ip, port);
-        fwrite(src, data);
-    case {'ExpEnd', 'ExpInterrupt'}
-        obj.stopExperiment;
-        timeStamp = datestr(clock, 'yyyy-mm-dd HH:MM:SS.FFF');
-        fprintf('[galvoUDP] [%s] Sending ''%s ...'' to %s:%d\n', timeStamp, info.instruction, ip, port);
-        fwrite(src, data);
-    case 'ZapPrepare'
-        obj.iTrial = info.iTrial;
-        obj.prepareNextStim(info)
-        timeStamp = datestr(clock, 'yyyy-mm-dd HH:MM:SS.FFF');
-        fprintf('[galvoUDP] [%s] Sending ''%s ...'' to %s:%d\n', timeStamp, info.instruction, ip, port);
-        fwrite(src, data);
-    case 'ZapStart'
-        obj.startZapping;
-        pause(0.1);
-        obj.logCameraFrame;
-        timeStamp = datestr(clock, 'yyyy-mm-dd HH:MM:SS.FFF');
-        fprintf('[galvoUDP] [%s] Sending ''%s ...'' to %s:%d\n', timeStamp, info.instruction, ip, port);
-        fwrite(src, data);
-    case 'ZapStop'
-        obj.stopZapping;
-        timeStamp = datestr(clock, 'yyyy-mm-dd HH:MM:SS.FFF');
-        fprintf('[galvoUDP] [%s] Sending ''%s ...'' to %s:%d\n', timeStamp, info.instruction, ip, port);
-        fwrite(src, data);
-    otherwise
-        fprintf('Unknown instruction : %s\n', info.instruction);
-        timeStamp = datestr(clock, 'yyyy-mm-dd HH:MM:SS.FFF');
-        fprintf('[galvoUDP] [%s] Sending ''%s ...'' to %s:%d\n', timeStamp, info.instruction, ip, port);
-        fwrite(src, data);
+    t = clock;
+    obj.nUDPs = obj.nUDPs + 1;
+    timeStamp = datestr(t, 'yyyy-mm-dd HH:MM:SS.FFF');
+    ip=src.DatagramAddress;
+    port=src.DatagramPort;
+    data=fread(src);
+    str=char(data');
+    obj.UDPLog(obj.nUDPs).timeStamp = datenum(t);
+    obj.UDPLog(obj.nUDPs).msg = str;
+    fprintf('[galvoUDP] [%s] Received \n%sfrom %s:%d\n', timeStamp, str, ip, port);
+    
+    try
+        info=parseMessage(str);
+    catch
+        info.instruction = 'Failed to parse';
+    end
+    
+    % update remote IP to that of the sender
+    src.RemoteHost = ip;
+    src.RemotePort = port;
+    
+    switch info.instruction
+        case 'hello'
+            % this is just to check communication
+            timeStamp = datestr(clock, 'yyyy-mm-dd HH:MM:SS.FFF');
+            fprintf('[galvoUDP] [%s] Sending ''%s ...'' to %s:%d\n', timeStamp, info.instruction, ip, port);
+            fwrite(src, data);
+        case 'ExpStart'
+            obj.startExperiment(info.ExpRef);
+            timeStamp = datestr(clock, 'yyyy-mm-dd HH:MM:SS.FFF');
+            fprintf('[galvoUDP] [%s] Sending ''%s ...'' to %s:%d\n', timeStamp, info.instruction, ip, port);
+            fwrite(src, data);
+        case {'ExpEnd', 'ExpInterrupt'}
+            obj.stopExperiment;
+            timeStamp = datestr(clock, 'yyyy-mm-dd HH:MM:SS.FFF');
+            fprintf('[galvoUDP] [%s] Sending ''%s ...'' to %s:%d\n', timeStamp, info.instruction, ip, port);
+            fwrite(src, data);
+        case 'ZapPrepare'
+            obj.iTrial = info.iTrial;
+            obj.prepareNextStim(info)
+            timeStamp = datestr(clock, 'yyyy-mm-dd HH:MM:SS.FFF');
+            fprintf('[galvoUDP] [%s] Sending ''%s ...'' to %s:%d\n', timeStamp, info.instruction, ip, port);
+            fwrite(src, data);
+        case 'ZapStart'
+            obj.startZapping;
+            pause(0.1);
+            obj.logCameraFrame;
+            timeStamp = datestr(clock, 'yyyy-mm-dd HH:MM:SS.FFF');
+            fprintf('[galvoUDP] [%s] Sending ''%s ...'' to %s:%d\n', timeStamp, info.instruction, ip, port);
+            fwrite(src, data);
+        case 'ZapStop'
+            obj.stopZapping;
+            timeStamp = datestr(clock, 'yyyy-mm-dd HH:MM:SS.FFF');
+            fprintf('[galvoUDP] [%s] Sending ''%s ...'' to %s:%d\n', timeStamp, info.instruction, ip, port);
+            fwrite(src, data);
+        otherwise
+            fprintf('Unknown instruction : %s\n', info.instruction);
+            timeStamp = datestr(clock, 'yyyy-mm-dd HH:MM:SS.FFF');
+            fprintf('[galvoUDP] [%s] Sending ''%s ...'' to %s:%d\n', timeStamp, info.instruction, ip, port);
+            fwrite(src, data);
+    end
+    
+catch e
+    warning('The following error caught in galvoCallback:\n');
+    warning(e.message);
+    % even when catching the error the DataGramReceivedFcn gets disabled
+    % sometimes (after 2-3 times)
+    % preemptively re-enabling the callback
+    fclose(obj.galvoUDP); 
+    fopen(obj.galvoUDP)
+    snd = load('laughter');
+    sound(snd.y, snd.Fs);
 end
 
 end
